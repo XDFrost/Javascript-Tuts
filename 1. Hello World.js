74f762d939824b458dfb0dea7a08b8ffc19917e9
XDFrost <pratham0gupta@gmail.com>
2023-06-16 21:27:28 +0530

    update

--- 1. Hello World.js
+++ 1. Hello World.js
@@ -1,1 +1,2 @@
-console.log("Hello World");        //console -> Used to print anything on console
+console.log("Hello World");        // console -> Used to print anything on console
+document.write("Hello World");          // Writes on the page
